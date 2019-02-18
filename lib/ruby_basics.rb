bbc12ed94d9bf234b3ceb37fc610d0c6bbde920b
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-02-18 00:31:38 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  div_num = num1 / num2
+  return div_num
 end
 
 def assign_variable(value)
-  
+  var = "#{value}"
+  return var
 end
 
-def argue
-  
+def argue (name)
+  name
 end
 
-def greeting
-
+def greeting (name, age)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party (topping = "cheese")
+  topping
 end
