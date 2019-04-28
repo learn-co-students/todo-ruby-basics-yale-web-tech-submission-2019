b624143308fb66c0a81921a45b4ad0c6912d3827
precious olisekwu <precious.olisekwu001@umb.edu>
2019-04-28 01:23:20 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,18 +1,26 @@
 def division(num1, num2)
- 
+  num1 / num2
+  num1 = 12
+  num2 = 2
+puts division(num1, num2)
 end
+division #{num1, num2}
 
 def assign_variable(value)
+puts assign_variable(20)  
   
 end
+assign_variable #{20}
 
 def argue
   
 end
 
-def greeting
+def greeting(greeting, name)
+puts greeting("Hello, Precious")
 
 end
+greeting #{Hello, Precious}
 
 def return_a_value
 
