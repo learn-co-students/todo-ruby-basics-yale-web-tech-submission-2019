bff3e25cda7b0e84629160b3cc415c475c88f1b4
precious olisekwu <precious.olisekwu001@umb.edu>
2019-04-28 01:29:49 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -7,14 +7,16 @@ end
 division #{num1, num2}
 
 def assign_variable(value)
-puts assign_variable(20)  
+puts assign_variable("Precious")  
   
 end
-assign_variable #{20}
+assign_variable #{Precious}
 
 def argue
+puts argue ("Ruby is a good language")
   
 end
+argue #{Ruby is a good language}
 
 def greeting(greeting, name)
 puts greeting("Hello, Precious")
@@ -23,13 +25,19 @@ end
 greeting #{Hello, Precious}
 
 def return_a_value
+puts return_a_value("Nice")  
 
 end
+return_a_value #{Nice}
 
 def last_evaluated_value
+puts last_evaluated_value("expert")
 
 end
+last_evaluated_value #{expert}
 
 def pizza_party
+puts pizza_party("cheese")  
 
 end
+pizza_party #{cheese}
