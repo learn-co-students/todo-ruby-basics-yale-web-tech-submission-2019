c242f53f86ea594a407ae251c23b1ad1e9f32aac
legacy-proxy-5746 <b.li@yale.edu>
2019-05-04 06:04:53 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  x = value
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
-
+def greeting(arg1, arg2)
+  arg1
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "Hello"
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
