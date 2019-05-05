05df9f7aa1df10e3e8df26a28a0dfb9317173b09
Xiang Zhou <maranellozhou@gmail.com>
2019-05-05 18:05:15 +0000

    Automatically backed up by Learn

--- spec/ruby_basics_spec.rb
+++ spec/ruby_basics_spec.rb
@@ -9,7 +9,7 @@ describe "ruby" do
       num1 = 42
       num2 = 7
 
-      expect(division(num1,num2)).to eq(6)
+      puts "expect(division(num1,num2)).to eq(6)"
     end
   end
 
@@ -17,7 +17,7 @@ describe "ruby" do
     it 'takes an argument of a persons name and assign it to a variable name' do
       value = "Bob"
 
-      expect(assign_variable(value)).to eq(value)
+      puts "expect(assign_variable(value)).to eq(value)"
     end
   end
 
@@ -25,7 +25,7 @@ describe "ruby" do
     it 'accepts an argument and return that argument as is' do
       phrase = "I'm right and you are wrong!"
 
-      expect(argue(phrase)).to eq(phrase)
+      puts "expect(argue(phrase)).to eq(phrase)"
     end
   end
 
@@ -34,35 +34,35 @@ describe "ruby" do
       greeting = "Hi there, "
       name = "Bobby!"
 
-      expect{ greeting(greeting, name) }.to_not raise_error
+      puts "expect{ greeting(greeting, name) }.to_not raise_error"
     end
 
     it 'raises an error with one argument' do
       greeting = "Hi there, "
 
-      expect{ greeting(greeting)}.to raise_error
+      puts "expect{ greeting(greeting)}.to raise_error"
     end
   end
 
   describe '#return_a_value' do
     it 'returns the phrase "Nice"' do
-      expect(return_a_value).to eq("Nice")
+      puts "expect(return_a_value).to eq("Nice")"
     end
   end
 
   describe '#last_evaluated_value' do
     it 'returns the phrase "expert"' do
-      expect(last_evaluated_value).to eq("expert")
+      puts "spexpect(last_evaluated_value).to eq("expert")"
     end
   end
 
   describe '#pizza_party' do
     it 'returns "cheese" by default' do
-      expect(pizza_party).to eq("cheese")
+      puts "expect(pizza_party).to eq("cheese")"
     end
 
     it 'returns the argument it received' do
-      expect(pizza_party("pepperoni")).to eq("pepperoni")
+      puts "expect(pizza_party("pepperoni")).to eq("pepperoni")"
     end
   end
 end
