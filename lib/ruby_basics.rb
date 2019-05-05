ca524e7379923f506d7a913c9de3dc86d92063fe
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-05 19:14:25 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+  quotient = num1/num2
+  quotient
 end
 
 def assign_variable(value)
-  
+  variable = value
+  variable
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
-
+def greeting(greeting, name)
+  puts "#{greeting} #{name}"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(flavor = "cheese")
+  flavor
 end
